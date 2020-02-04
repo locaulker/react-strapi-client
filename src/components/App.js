b8651c8eab69a7ee47ac3ae17a43ddfdb76f34d3
Lawrence <Caulker>
2020-02-03 22:20:51 -0500

    finished search box

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
-import React, { Component } from "react";
-import { Container, Box, Heading, Card, Image } from 'gestalt'
-import "./App.css";
+import React, { Component } from "react"
+import { Container, Box, Heading, Card, Image, Text, SearchField, Icon, Spinner } from 'gestalt'
+import { Link } from 'react-router-dom'
+import "./App.css"
 import Strapi from 'strapi-sdk-javascript/build/main'
+import Loader from './Loader'
 
 const apiUrl = process.env.API_URL || "http://localhost:1337"
 const strapi = new Strapi(apiUrl)
@@ -9,7 +11,9 @@ const strapi = new Strapi(apiUrl)
 class App extends Component {
 
   state = {
-    brands: []
+    brands: [],
+    searchTerm: '',
+    loadingBrands: true
   }
 
   async componentDidMount() {
@@ -28,18 +32,49 @@ class App extends Component {
           }`
         }
       })
-      // console.log(response)
-      this.setState({ brands: response.data.brands })
+      console.log(response)
+      this.setState({ brands: response.data.brands, loadingBrands: false })
     } catch (err) {
       console.log(err)
+      this.setState({ loadingBrands: false })
     }
   }
 
+  handleChange = ({ value }) => {
+    this.setState({ searchTerm: value })
+  }
+
+  filteredBrands = ({ searchTerm, brands }) => {
+    return brands.filter(brand => {
+      return (brand.name.toLowerCase().includes(searchTerm.toLowerCase()) || brand.description.toLowerCase().includes(searchTerm.toLowerCase()))
+    })
+  }
+
   render() {
-    const { brands } = this.state
+    const { searchTerm, loadingBrands } = this.state
 
     return (
       <Container>
+        {/* Brands Search field */}
+        <Box display="flex" justifyContent="center" marginTop={4}>
+          <SearchField
+            id="searchField"
+            accessibilityLabel="Brands Search Field"
+            onChange={this.handleChange}
+            value={searchTerm}
+            placeholder="Search for Brands"
+          />
+          <Box margin={3}>
+            <Icon
+              icon="filter"
+              color={searchTerm ? 'orange' : 'gray'}
+              size={20}
+              accessibilityLabel="Filter"
+            />
+          </Box>
+        </Box>
+
+        {/* Brands Search */}
         <Box
           display="flex"
           justifyContent="center"
@@ -51,15 +86,21 @@ class App extends Component {
           </Heading>
         </Box>
         {/* Brands */}
-        <Box display="flex" justifyContent="around">
-          {brands.map(brand => (
-            <Box
-              key={brand._id}
-            >
+        <Box
+          dangerouslySetInlineStyle={{
+            __style: {
+              backgroundColor: '#d6c8ec'
+            }
+          }}
+          shape="rounded"
+          wrap display="flex" justifyContent="around">
+          {this.filteredBrands(this.state).map(brand => (
+            <Box padding={4} margin={2} width={200} key={brand._id}>
               <Card
                 image={
                   <Box height={200} width={200}>
                     <Image
+                      fit="cover"
                       alt="Brand"
                       naturalHeight={1}
                       naturalWidth={1}
@@ -68,14 +109,28 @@ class App extends Component {
                   </Box>
                 }
               >
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  justifyContent="center"
+                  direction="column"
+                >
+                  <Text bold size="xl">{brand.name}</Text>
+                  <Text>{brand.description}</Text>
+                  <Text bold size="xl">
+                    <Link to={`/${brand._id}`}>See Brews</Link>
+                  </Text>
+                </Box>
 
               </Card>
             </Box>
           ))}
         </Box>
+        {/* <Spinner show={loadingBrands} accessibilityLabel="Loading Spinner" /> */}
+        <Loader show={loadingBrands} />
       </Container>
     )
   }
 }
 
-export default App;
+export default App
